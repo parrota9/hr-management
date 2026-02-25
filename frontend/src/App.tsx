b6200f474fe795b301fc6f71117cdb7a42f6e944
zakyh <zaky.hakim@gmail.com>
2026-02-25 22:41:33 +0800

    Initial commit: FastAPI + React TS setup with Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h2 className="text-2xl font-bold text-center mt-10 text-red-200">
+        HR Management System
+      </h2>
+    </>
+  );
+}
+
+export default App;
